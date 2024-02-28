ed583e79b1a1fa9e966af24d224777a9c6a258d4
tonni-akter-dev <tanniakhter123@gmail.com>
2024-02-28 16:15:20 +0600

    DSF

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -3,6 +3,7 @@ import "../globals.scss";
 import TopBar from "@/components/TopBar/TopBar";
 import { DriverProvider } from '../../hooks/driverContext'
 import { TruckProvider } from "@/hooks/truckContext";
+
 export const metadata = {
   title: {
     template: "%s | Truck Authorization System",
@@ -19,18 +20,16 @@ export default function DashboardLayout({
     <html lang="en">
       <TruckProvider>
         <DriverProvider>
-
           <body>
             <Sidebar />
             <div className="lg:ms-[296px] ms-[50px] w-full">
               <div className="w-full">
                 <TopBar />
               </div>
-              <div className="mt-[100px] bg-[]  ">{children}</div>
+              <div className="mt-[100px] bg-[] ">{children}</div>
             </div>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.js"></script>
+            <script async src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.js"></script>
           </body>
-
         </DriverProvider>
       </TruckProvider>
     </html>
