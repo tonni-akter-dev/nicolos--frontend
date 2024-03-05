6f9e2b627d0063967456376ce8e3600609500a6f
tonni-akter-dev <tanniakhter123@gmail.com>
2024-03-05 17:57:03 +0600

    update

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -12,12 +12,11 @@ const Dashboard = () => {
         <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.css" rel="stylesheet" />
         <script async src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.js" />
       </Head>
-      <body>
 
-        <div className="dashboard">
-          <Stats />
-        </div>
-      </body>
+      <div className="dashboard">
+        <Stats />
+      </div>
+
     </>
   )
 }
